cb3844fedc7228af4a072e204260bdb0f74d7760
ce314007 <simanjuntakyosep190@gmail.com>
2017-11-14 14:38:18 +0700

    Update PromblemTest2.py

--- PromblemTest2.py
+++ PromblemTest2.py
@@ -2,7 +2,7 @@ angka1, angka2 = 0, 1
 total = 0
 while True:
     angka1, angka2 = angka2, angka1 + angka2
-    if cur >= 4000000:
+    if angka2 >= 4000000:
         break
     if angka2 % 2 == 0:
         total += angka2
