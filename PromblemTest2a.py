159fb18e2e0b60e9b1abb6f1f366b34511c92f4c
ce314007 <simanjuntakyosep190@gmail.com>
2017-11-14 14:40:26 +0700

    latihan 2

---
+++ PromblemTest2a.py
@@ -0,0 +1,9 @@
+angka1, angka2 = 0, 1
+total = 0
+while True:
+    angka1, angka2 = angka2, angka1 + angka2
+    if angka2 >= 4000000:
+        break
+    if angka2 % 2 == 0:
+        total += angka2
+print(total)
