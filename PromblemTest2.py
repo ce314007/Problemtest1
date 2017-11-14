f0a91c2293b5091c9b82d436e459fe064e155a34
ce314007 <simanjuntakyosep190@gmail.com>
2017-11-14 14:37:39 +0700

    Update PromblemTest2.py

--- PromblemTest2.py
+++ PromblemTest2.py
@@ -1,9 +1,9 @@
-prev, cur = 0, 1
+angka1, angka2 = 0, 1
 total = 0
 while True:
-    prev, cur = cur, prev + cur
+    angka1, angka2 = angka2, angka1 + angka2
     if cur >= 4000000:
         break
-    if cur % 2 == 0:
-        total += cur
+    if angka2 % 2 == 0:
+        total += angka2
 print(total)
